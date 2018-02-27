42414d318f849584b17a7c6cae701caa32b2a657
Gregory <dimac2278@yandex.ru>
2018-02-27 13:08:44 +0300

    task8

--- src/OrientedGraph.java
+++ src/OrientedGraph.java
@@ -1,9 +1,11 @@
+import com.sun.org.apache.xpath.internal.operations.Or;
+
 import java.util.*;
 public class OrientedGraph {
     private ArrayList<ArrayList<Integer>> matrix;
-    private ArrayList<String> vertex;
+    private ArrayList<Integer> vertex;
 
-    public ArrayList<String> getVertex() {
+    public ArrayList<Integer> getVertex() {
         return vertex;
     }
 
@@ -11,7 +13,7 @@ public class OrientedGraph {
         return matrix;
     }
 
-    public Integer[][] matrixOfGraph(ArrayList<ArrayList<Integer>> matrix, ArrayList<String> vertex) {
+    private static Integer[][] matrixOfGraph(ArrayList<ArrayList<Integer>> matrix, ArrayList<Integer> vertex) {
         Integer size = vertex.size();
         Integer[][] marx = new Integer[size][size];
         for (ArrayList<Integer> helpList : matrix) {
@@ -20,8 +22,33 @@ public class OrientedGraph {
         return marx;
     }
 
+     public void addVertex(Integer newVertex) {
+        vertex.add(newVertex);
+     }
+
+     public void addArc(ArrayList<Integer> newArc) {
+        matrix.add(newArc);
+     }
+
+     public void deleteVertex(Integer oldVertex) {
+        vertex.remove(oldVertex);
+     }
+
+     public void deleteArc(ArrayList<Integer> oldArc){
+        matrix.remove(oldArc);
+     }
+      public void renameVertex(Integer position, Integer name) {
+
+      }
 
     public static void main(String[] args) {
+        ArrayList<Integer> newList = new ArrayList<>();
+        newList.add(1);
+        newList.add(2);
+        newList.add(3);
+        OrientedGraph lel = new OrientedGraph();
+        lel.addVertex(1);
+        lel.addArc(newList);
 
     }
 }
